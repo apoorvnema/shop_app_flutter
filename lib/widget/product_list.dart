3baff32d0827df6665623f3f38cbd1612d7ad385
apoorvnema <apoorvnema38@gmail.com>
2025-09-10 23:49:17 +0530

    UPDATE: Replace deprecated MediaQuery.of(context).size with MediaQuery.sizeOf(context)

--- lib/widget/product_list.dart
+++ lib/widget/product_list.dart
@@ -21,7 +21,7 @@ class _ProductListState extends State<ProductList> {
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
+    final size = MediaQuery.sizeOf(context);
     const border = OutlineInputBorder(
       borderSide: BorderSide(color: Color.fromARGB(255, 255, 255, 1)),
       borderRadius: BorderRadius.horizontal(left: Radius.circular(50)),
